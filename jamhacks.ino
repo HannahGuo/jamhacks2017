2cc0c83dc843d320ed4a70c7002cb3b249a34eea
HannahGuo <hguo2102@gmail.com>
2017-04-09 17:38:07 -0400

    Working sudden movement detection

--- jamhacks.ino
+++ jamhacks.ino
@@ -24,72 +24,29 @@ ADXL345 adxl; //variable adxl is an instance of the ADXL345 library
 //NfcAdapter nfc = NfcAdapter(pn532_i2c);
 int X_Read, Y_Read, Z_Read;
 double ax, ay, az;
-double start = 0;
 boolean hasFallen = false, alerted = false;
 
 void setup() {
   Wire.begin();
   Serial.begin(9600);
   delay(100);
-  //Turning on the ADXL345
-  adxl.powerOn();
-
-  //set activity/ inactivity thresholds (0-255)
-  adxl.setActivityThreshold(75); //62.5mg per increment
-  adxl.setInactivityThreshold(75); //62.5mg per increment
-  adxl.setTimeInactivity(10); // how many seconds of no activity is inactive?
- 
-  //look of activity movement on this axes - 1 == on; 0 == off 
-  adxl.setActivityX(1);
+  adxl.powerOn();   //Turning on the ADXL345
+  adxl.setActivityX(1);  //look of activity movement on this axes - 1 == on; 0 == off
   adxl.setActivityY(1);
   adxl.setActivityZ(1);
- 
-  //look of inactivity movement on this axes - 1 == on; 0 == off
-  adxl.setInactivityX(1);
-  adxl.setInactivityY(1);
-  adxl.setInactivityZ(1);
- 
-  //look of tap movement on this axes - 1 == on; 0 == off
-  adxl.setTapDetectionOnX(0);
-  adxl.setTapDetectionOnY(0);
-  adxl.setTapDetectionOnZ(1);
- 
-  //set values for what is a tap, and what is a double tap (0-255)
-  adxl.setTapThreshold(50); //62.5mg per increment
-  adxl.setTapDuration(15); //625us per increment
-  adxl.setDoubleTapLatency(80); //1.25ms per increment
-  adxl.setDoubleTapWindow(200); //1.25ms per increment
- 
-  //set values for what is considered freefall (0-255)
-  adxl.setFreeFallThreshold(7); //(5 - 9) recommended - 62.5mg per increment
-  adxl.setFreeFallDuration(45); //(20 - 70) recommended - 5ms per increment
- 
-  //setting all interrupts to take place on int pin 1
-  //I had issues with int pin 2, was unable to reset it
-  adxl.setInterruptMapping( ADXL345_INT_SINGLE_TAP_BIT,   ADXL345_INT1_PIN );
-  adxl.setInterruptMapping( ADXL345_INT_DOUBLE_TAP_BIT,   ADXL345_INT1_PIN );
-  adxl.setInterruptMapping( ADXL345_INT_FREE_FALL_BIT,    ADXL345_INT1_PIN );
-  adxl.setInterruptMapping( ADXL345_INT_ACTIVITY_BIT,     ADXL345_INT1_PIN );
-  adxl.setInterruptMapping( ADXL345_INT_INACTIVITY_BIT,   ADXL345_INT1_PIN );
- 
-  //register interrupt actions - 1 == on; 0 == off  
-  adxl.setInterrupt( ADXL345_INT_SINGLE_TAP_BIT, 1);
-  adxl.setInterrupt( ADXL345_INT_DOUBLE_TAP_BIT, 1);
-  adxl.setInterrupt( ADXL345_INT_FREE_FALL_BIT,  1);
-  adxl.setInterrupt( ADXL345_INT_ACTIVITY_BIT,   1);
-  adxl.setInterrupt( ADXL345_INT_INACTIVITY_BIT, 1);
+  pinMode(11, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   X_Read = readRegister(ADXL345_ADDRESS, ADXL345_DATAX0, ADXL345_DATAX1);
   Y_Read = readRegister(ADXL345_ADDRESS, ADXL345_DATAY0, ADXL345_DATAY1);
-  Z_Read = readRegister(ADXL345_ADDRESS, ADXL345_DATAZ0, ADXL345_DATAZ1);  
+  Z_Read = readRegister(ADXL345_ADDRESS, ADXL345_DATAZ0, ADXL345_DATAZ1);
   getAcceleration();
-  detectFall();
+//  buzzerToggle();
   if (hasFallen && !alerted) {
     alert();
   }
-  buzzerToggle();
 }
 
 int readRegister(int deviceAddress, int address1, int address2) {
@@ -104,7 +61,6 @@ int readRegister(int deviceAddress, int address1, int address2) {
     readValue1 = Wire.read();
     readValue2 = Wire.read();
   }
-  //Wire.endTransmission();
   readValue2 = readValue2 << 8;
   value = readValue1 + readValue2;
   delay(100);
@@ -117,53 +73,31 @@ void getAcceleration() {
   ay = Y_Read * gains;
   az = Z_Read * gains;
 
-   //Boring accelerometer stuff   
-  int x,y,z;  
+  //Boring accelerometer stuff
+  int x, y, z;
   adxl.readXYZ(&x, &y, &z); //read the accelerometer values and store them in variables  x,y,z
-  // Output x,y,z values 
+  // Output x,y,z values
   Serial.print("values of X , Y , Z: ");
   Serial.print(x);
   Serial.print(" , ");
   Serial.print(y);
   Serial.print(" , ");
   Serial.println(z);
-  
-  double xyz[3];
-  double ax,ay,az;
-  adxl.getAcceleration(xyz);
-  ax = xyz[0];
-  ay = xyz[1];
-  az = xyz[2];
-  Serial.print("X=");
-  Serial.print(ax);
-    Serial.println(" g");
-  Serial.print("Y=");
-  Serial.print(ay);
-    Serial.println(" g");
-  Serial.print("Z=");
-  Serial.println(az);
-    Serial.println(" g");
-  Serial.println("**********************");
-  delay(500);
-
-   byte interrupts = adxl.getInterruptSource();
-   if(adxl.triggered(interrupts, ADXL345_FREE_FALL)){
-    Serial.println("freefall");
-    //add code here to do when freefall is sensed
-  } 
+  detectFall(x);
 }
 
-void detectFall() {
-  double prev = start;
-  double current = 0;
-  if (current - prev < 1 && current == 0) {
+void detectFall(double ax) {
+  if (ax > 50000 && !hasFallen) {
     hasFallen = true;
+    Serial.println("FALL DETECTED AHHHHHHHH!");
   }
 }
 
 void alert() {
-  buzzerToggle();
-  cancelAlert();
+  while (hasFallen && !alerted) {
+    buzzerToggle();
+    cancelAlert();
+  }
 }
 
 void cancelAlert() {
@@ -175,6 +109,7 @@ void cancelAlert() {
 }
 
 void buzzerToggle() {
+//  Serial.print("BUZZER HAS BEEN ACTIVATED");
   digitalWrite(11, HIGH);
   digitalWrite(13, HIGH);
   delayMicroseconds(150);
@@ -188,29 +123,3 @@ void reset() {
   digitalWrite(11, LOW);
   digitalWrite(13, LOW);
 }
-
-/*****************************************************************************/
-//  Function:    Get the accelemeter of X/Y/Z axis and print out on the 
-//          serial monitor.
-//  Hardware:    3-Axis Digital Accelerometer(��16g)
-//  Arduino IDE: Arduino-1.0
-//  Author:  Frankie.Chu    
-//  Date:    Jan 11,2013
-//  Version: v1.0
-//  by www.seeedstudio.com
-//
-//  This library is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU Lesser General Public
-//  License as published by the Free Software Foundation; either
-//  version 2.1 of the License, or (at your option) any later version.
-//
-//  This library is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public
-//  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-//
-/*******************************************************************************/
